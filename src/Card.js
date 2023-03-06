4c03530d534fb2394ecfc630a0ef2cdf871a0b63
João Martins <joao.martins.ext@spms.min-saude.pt>
2023-03-06 10:30:09 +0000

    Alteração shadow card

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({id, name, email}) => {
     return (
-        <div className='dib pa3 ma2 br2 dib bg-light-green tc grow'>
+        <div className='dib pa3 ma2 br2 dib bg-light-green shadow-1 tc grow'>
             <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
             <div>
                 <h2>{name}</h2>
